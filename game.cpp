da4b99c7c6a97fb1f295eabcb612b1b1f020df6c
bregus <iromka74@gmail.com>
2016-11-12 01:04:43 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -93,132 +93,126 @@ void print_end_of_chose ()
 			cout << "You set your main stats!";
 			cout << endl <<"=========================================="<<endl;
 }
+void choose_stats(human &data) {
+	int points = 5;
+	int Strenght = 0 , Agility = 0, Stamina = 0;
+	cout << "\nChose amount of your main characteristics (Strenght, Agility, Stamina):" << endl;
+	printf("You have %d points. \n", points);
+	cout << "Strenght: "; cin >> Strenght;
+	Set_points (Strenght, points);
+		
+	if (points != 0)
+	{
+		printf("You have %d points. \n", points);
+		cout << "Agility: ";  cin >> Agility;
+		Set_points (Agility, points);
+	}
+	if (points != 0)
+	{
+		printf("You have %d points. \n", points);
+		cout << "Stamina: "; cin >> Stamina;
+		Set_points (Stamina, points);
+	}
+	data.Set_Stats(Strenght, Agility, Stamina);
+}
 
 int main ()
 {
-	srand( time( 0 ) );
+	srand(time(0));
 	int var;
-	human var1;
-	human var2;
-	bool Set_var1 = true;
-	for (int i = 1; i <= 2;i++ )
-	{
-		int points = 5;
-		int Strenght = 0 , Agility = 0, Stamina = 0;
-		cout << "Chose amount of your main characteristics:" << endl;
-		printf("You have %d points \n", points);
-		cout << "Strenght: "; cin >> Strenght;
-		Set_points (Strenght, points);
-			
-		if (points != 0)
-		{
-			cout << "Agility: ";  cin >> Agility;
-			Set_points (Agility, points);
-		}
-		if (points != 0)
-		{
-			cout << "Stamina: "; cin >> Stamina;
-			Set_points (Stamina, points);
-		}
-
-		
-
-		print_end_of_chose();
-		if (Set_var1)
-		{var1.Set_Stats(Strenght, Agility, Stamina); Set_var1 = false;}
-		else 
-			var2.Set_Stats(Strenght, Agility, Stamina);
-	}
-
-
-	cout << "          1 player     2 player"<<endl;
-	cout << "Strenght:      " << var1.Strenght() << "       " << var2.Strenght()<<endl;
-	cout << "Agility:       " << var1.Agility() << "       " << var2.Agility()<<endl;
-	cout << "Stamina:       " << var1.Stamina() << "       " << var2.Stamina()<<endl;
-	  
-	while(true)
-	{
-		if ((var1.hp()<= 0) || (var2.hp()<= 0))
-		{
-			cout << endl <<"=========================================="<<endl;
-			cout << "               !!!YOU WIN!!! ";
-			cout << endl <<"=========================================="<<endl;
-			break;
-		}
-		cout << "var1 :" << endl;
-		cout << "hp = " << var1.hp() << "  " << "Energy = " << var1.Energy() << endl;
-
-		cout << "var2 :" << endl;
-		cout << "hp = " << var2.hp() << "  " << "Energy = " << var2.Energy() << endl;
-
-		if (fight_1)
-		{cout << "........HODIT 1............ "<< endl;}
-		else
-		{cout << "........HODIT 2............ "<< endl;}
-		//cin >> var;
-		var = 1;
-		switch(var)
-		{
-		case 1: 
-			{
-					if (fight_1)
-					{
-						fight_1 = !fight_1;
-						if (rand() % 101 < var2.Dodge())
-						{
-							cout << endl <<"=========================================="<<endl;
-							cout << "                MISS           ";
-							cout << endl <<"=========================================="<<endl;
-							break;
-						}
-						var2.Hit_hp(var1.Hit());
-						cout << "\n       HIT : " << var1.Hit() << endl;
-					}
-					else 
-					{	
-						fight_1 = !fight_1;
-						if (rand() % 101 < var1.Dodge())
-						{
-							cout << endl <<"=========================================="<<endl;
-							cout << "                MISS           ";
-							cout << endl <<"=========================================="<<endl;
-							break;	
-						}
-						var1.Hit_hp(var2.Hit());
-						cout << "\n       HIT : " << var2.Hit() << endl;
-				    }
-				break;
-			}
-		case 2:{
-				if((rand() % 101) < 1)
-						{
-							cout << endl <<"=========================================="<<endl;
-							cout << "             MISS             ";
-							cout << endl <<"=========================================="<<endl;
-							fight_1 = !fight_1;
-							break;
-						}
-				else
-				{	if(fight_1)
-					{
-						var2.Hit_Magic(Hit_Magic_hp);
-						var1.Energy_sub(Mana);
-						var2.Hit_hp(var1.Hit());
-						fight_1 = !fight_1;
-					}
-					else
-					{
-						var1.Hit_Magic(Hit_Magic_hp);
-						var2.Energy_sub(Mana);
-						var1.Hit_hp(var2.Hit());
-						fight_1 = !fight_1;
-					}
-				}
-				break;
-			   }
-
-		}
 	
+	while(true) {
+		cout << "1. Play.\n";
+		cout << "2. Exit.\n";
+		cout << ">>>>>>";
+		cin >> var;
+		switch (var) 
+		{
+			case 1: 
+			{	
+				human var1;
+				human var2;
+				
+				choose_stats(var1);
+				choose_stats(var2);
+				print_player_stats(var1, var2);
+	
+				while(true)
+				{
+		
+					if (var1.hp() <= 0)
+					{
+						cls();
+						cout << endl <<"=========================================="<<endl;
+						cout << "               !!!Player Two Win!!! ";
+						cout << endl <<"=========================================="<<endl;
+						break;
+					}
+
+					if (var2.hp() <= 0)
+					{
+						cls();
+						cout << endl <<"=========================================="<<endl;
+						cout << "               !!!Player One Win!!! ";
+						cout << endl <<"=========================================="<<endl;
+						break;
+					}
+					cout << "var1 :" << endl;
+					cout << "hp = " << var1.hp() << "  " << "Energy = " << var1.Energy() << endl;
+
+					cout << "var2 :" << endl;
+					cout << "hp = " << var2.hp() << "  " << "Energy = " << var2.Energy() << endl;
+
+					if (fight_1)
+					{cout << "........First player turn............ "<< endl;}
+					else
+					{cout << "........Second player turn........... "<< endl;}
+					//cin >> var;
+					var = 1;
+					switch(var)
+					{
+						case 1: 
+						{
+							//cls();
+							if (fight_1)
+							{
+								fight_1 = !fight_1;
+								if (rand() % 101 < var2.Dodge())
+								{
+									cout << endl <<"=========================================="<<endl;
+									cout << "                MISS           ";
+									cout << endl <<"=========================================="<<endl;
+									break;
+								}
+								var2.Hit_hp(var1.Hit());
+								cout << "\n       HIT : " << var1.Hit() << endl;
+							}
+							else 
+							{	
+								fight_1 = !fight_1;
+								if (rand() % 101 < var1.Dodge())
+								{
+									cout << endl <<"=========================================="<<endl;
+									cout << "                MISS           ";
+									cout << endl <<"=========================================="<<endl;
+									break;	
+								}
+								var1.Hit_hp(var2.Hit());
+								cout << "\n       HIT : " << var2.Hit() << endl;
+					    	}
+						break;
+						}
+					}
+					break;
+				}
+				case 2: 
+				{
+					return 0;
+				}
+			}
+		}
 	}
+
+	
 	return 0;
 }
